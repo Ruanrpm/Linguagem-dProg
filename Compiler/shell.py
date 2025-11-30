26301e49179364271e9b253090b3fda497275457
Ruanrpm <ruan.pablo.martinz@gmail.com>
2025-11-30 13:26:16 -0300

    Estrutura switch funcinal

--- Compiler/shell.py
+++ Compiler/shell.py
@@ -1,6 +1,26 @@
-import bakaa
+import sys
+import rpm
 
-while True:
-    text = input('bakaa>>') 
-    tokens = bakaa.run(text)
-    print(tokens)
+def executarArquivo(caminhoArquivo):
+    if not caminhoArquivo.endswith('.rm'):
+        print("A extensão do arquivo não é .rm")
+        return
+    
+    try:
+        with open(caminhoArquivo, 'r') as arquivo:
+            texto = arquivo.read()
+    except FileNotFoundError:
+        print(f'Arquivo {caminhoArquivo} não encontrado')
+    
+    for numero, linha in enumerate(texto.split('\n'), start=1):
+        if (linha.strip() == ''):
+            continue
+
+        resultado, erro = rpm.run(caminhoArquivo, linha)
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 2):
+        print("Use: python shell.py <nomeArquivo.rm>")
+    else:
+        executarArquivo(sys.argv[1])
